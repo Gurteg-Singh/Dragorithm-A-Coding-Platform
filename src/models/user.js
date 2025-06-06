9bf59ef3e721ba809b304ce1dd974de13d43c833
Gurteg Singh <gurtegsohi123@gmail.com>
2025-06-06 13:30:29 -0400

    user schema made

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const { kMaxLength } = require("buffer");
+const mongoose = require("mongoose");
+const { type } = require("os");
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type : String,
+        required : true,
+        minLength : 3,
+        maxLenght : 15,
+        trim : true
+    },
+    lastName:{
+        type : String,
+        minLength : 3,
+        maxLenght : 15,
+        trim : true
+    },
+    email:{
+        type : String,
+        unique : true,
+        required : true,
+        trim : true,
+        immutable: true,
+        lowercase:true
+    },
+    password:{
+        type : String,
+        required : true,
+        minLenght : 8
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default: 'user'
+    },
+    problemsolved :{
+        type : [String] 
+    }
+},{timestamps:true});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
