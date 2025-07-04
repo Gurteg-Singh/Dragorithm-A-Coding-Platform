a67926de643df755b2b3a6f4e6626fcf37ab6283
Gurteg Singh <gurtegsohi123@gmail.com>
2025-07-04 12:56:17 -0400

    Debugging code

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,11 +7,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <Provider store={stores}>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
     </Provider>
-  </StrictMode>
+  // </StrictMode>
 )
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,15 +17,15 @@ export default function Home(){
         async function fetchProblems(){
             try{
                 const response = await axiosClient.get('/problem/getAllProblems');
-                setproblems(response);
+                setproblems(response.data);
             }catch(err){
                 console.log("NO PROBLEMS TO DISPLAY");
             }
         }
         async function fetchSolvedProblems(){
             try{
-                const response = await axiosClient.get('/problem//getAllProblemsSolvedByUser');
-                setsolvedProblems(response);
+                const response = await axiosClient.get('/problem/getAllProblemsSolvedByUser');
+                setsolvedProblems(response.data);
             }catch(err){
                 console.log("NO SOLVED PROBLEMS TO DISPLAY");
             }
@@ -35,35 +35,25 @@ export default function Home(){
             fetchSolvedProblems();
         }
     },[]);
+    console.log("ALL PROBLEMS : ")
+    console.log(problems);
+    console.log("\n");
+
+    console.log("SOLVED PROBLEMS : ")
+    console.log(solvedProblems);
 
     return(
-        <div className="h-screen w-screen bg-gray-100">
-            <nav className="h-16 w-full bg-blue-300">
+        <div>
+            <nav>
                 {/* navigation button */}
             </nav>
 
-            <div className="h-16 w-full bg-green-200">
+            <div>
                 {/* Filters */}
             </div>
 
-            <div className="p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-                {/* ALL PROBLEMS */}
-                {problems.map((problem) => (
-                    <div key={problem._id} className="bg-white shadow-md p-4 rounded-xl border border-gray-200">
-                        <h2 className="text-lg font-semibold">{problem.title}</h2>
-                        <p className="text-sm text-gray-600 capitalize mt-1">Difficulty: {problem.difficultyLevel}</p>
-                        <div className="flex flex-wrap gap-2 mt-2">
-                            {problem.tags.map((tag, idx) => (
-                                <span
-                                    key={idx}
-                                    className="bg-purple-100 text-purple-700 px-2 py-1 rounded-full text-xs"
-                                >
-                                    {tag}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                ))}
+            <div>
+                THIS IS HOME PAGE
             </div>
         </div>
     )
